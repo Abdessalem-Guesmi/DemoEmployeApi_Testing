34a828c11eb5e1d55963a00a39eb35b4253ca5c7
Abdessalem Guesmi <guesmiabdessalem@yahoo.fr>
2021-04-25 01:30:44 +0100

    second commit

--- src/main/java/com/abdou/web/EmployeController.java
+++ src/main/java/com/abdou/web/EmployeController.java
@@ -1,10 +1,14 @@
 package com.abdou.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,24 @@ public class EmployeController {
 	@PostMapping("/employe")
 	public Employe saveEmploye(@RequestBody Employe e) {
 		return employeRepository.save(e);
+	}
+
+	@GetMapping("/employe/{id}")
+	public Optional<Employe> getEmploye(@PathVariable Long id) {
+		return employeRepository.findById(id);
+	}
+
+	@DeleteMapping("/employe/{idEmploye}")
+	public boolean deleteEmploye(@PathVariable("idEmploye") Long id) {
+		employeRepository.deleteById(id);
+		return true;
+	}
+
+	@PutMapping("/employes/{id}")
+	public Employe updateEmploye(@RequestBody Employe employe, @PathVariable Long id) {
+		employe.setId(id);
+		return employeRepository.save(employe);
+
 	}
 
 }
